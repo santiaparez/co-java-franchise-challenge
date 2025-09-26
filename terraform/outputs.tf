cc112f9ff0b485b8b94aafa0bd5c281ec5a86e55
Santiago Apraez Henao <santiago.apraez@pragma.com.co>
2025-09-26 09:29:00 -0500

    add terraform

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,11 @@
-output "alb_dns" { value = "TODO" }
+output "public_ip" {
+  value = aws_instance.app.public_ip
+}
+
+output "public_dns" {
+  value = aws_instance.app.public_dns
+}
+
+output "app_url" {
+  value = "http://${aws_instance.app.public_ip}:${var.app_port}/actuator/health"
+}
